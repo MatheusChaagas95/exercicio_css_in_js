2760e2c1188f1e7d67a7c584515654b8597b7147
matheus-chagas <matheus.fernando.chagas@outlook.com.br>
2025-08-19 18:55:03 -0300

    commit final conserto de merge

--- src/components/Vagas/index.tsx
+++ src/components/Vagas/index.tsx
@@ -1,7 +1,7 @@
 import React, { useMemo } from 'react';
 import styled from 'styled-components';
 
-// Styled-components compactos
+
 const Container = styled.section`
   padding: 2rem 1rem;
   max-width: 1200px;
